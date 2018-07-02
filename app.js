c1c119e7b7936ab7c78f4f0b0bf53dd0b571801c
sensecherise <sensecherise@gmail.com>
2018-07-02 14:13:48 +0700

    my code

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log('start');
+
+const fs = require('fs')
+const os = require('os')
+
+var user = os.userInfo()
+
+console.log(user);
+fs.appendFile('greetings.txt', 'Hello : '+ user, function(err){
+    if(err){
+        console.log(err)
+    }
+});
