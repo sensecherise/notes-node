cadd8c77184e289f42981582d99a50c0ca378709
sensecherise <sensecherise@gmail.com>
2018-07-04 14:55:51 +0700

    4/7/18 14:55

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ console.log('start');
 
 const fs = require('fs')
 const os = require('os')
+const notes = require('./notes.js')
 
-var user = os.userInfo()
 
-console.log(user);
-fs.appendFile('greetings.txt', `Hello ${user.username}`, function(err){
-    if(err){
-        console.log(err);
-    };
-});
+var res = notes.addNote();
+console.log(res)
+
+var showNumber = notes.add(10, 12);
+console.log(showNumber)
+// var user = os.userInfo()
+
+//console.log(user);
+// fs.appendFile('greetings.txt', `Hello ${user.username} You are ${notes.age}`, function(err){
+//     if(err){
+//         console.log(err);
+//     };
+// });
 
 
---
+++ notes.js
@@ -0,0 +1,12 @@
+console.log('starting notes.js')
+
+console.log(module)
+module.exports.age = 25;
+module.exports.addNote = () => {
+    console.log('addNote')
+    return 'New Notes'
+}
+
+module.exports.add = (numA, numB) => {
+    return numA+numB
+}
