bfb415a2a8dd0609f88af7dc21dd3217387e2dd1
sensecherise <sensecherise@gmail.com>
2018-07-05 16:02:29 +0700

    edited: app.js and notes.js

--- app.js
+++ app.js
@@ -3,6 +3,26 @@ console.log('start app.js');
 
 const fs = require('fs')
 const _ = require('lodash')
+const yargs = require('yargs').argv
 
 const notes = require('./notes.js')
-
+var command = yargs._[0]
+console.log('command : ',command)
+console.log('Process', process.argv)
+console.log('Yargs', yargs)
+console.log(yargs.title)
+if( command === 'add'){
+    notes.addNote(yargs.title, yargs.body)
+}
+else if (command === 'list'){
+    notes.getAll();
+}
+else if( command === 'read'){
+    notes.getNote(yargs.title);
+}
+else if(command === 'remove'){
+    notes.removeNote(yargs.title);
+}
+else{
+    console.log('command not recognized')
+}
--- notes.js
+++ notes.js
@@ -2,7 +2,26 @@ console.log('starting notes.js')
 
 //console.log(module)
 module.exports.age = 25;
-module.exports.addNote = () => {
-    console.log('addNote')
-    return 'New Notes'
+
+var addNote = (title, body) => {
+    console.log('adding note', title, body);
+}
+
+var getAll = () => {
+    console.log('get all notes')
+}
+
+var getNote = (title) => {
+    console.log('getting note' , title)
+}
+
+var removeNote = (title) => {
+    console.log('note deleted', title)
+}
+
+module.exports = {
+    addNote,
+    getAll,
+    removeNote,
+    getNote
 }
